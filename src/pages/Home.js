425f955b020ef1d5e508b07c3c20cf074524dbd1
lenoryv <luisortiz141999@gmail.com>
2022-09-24 14:00:31 -0500

    Add new section Social Network

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
+import { Card } from '../components/Card'
+
 const Home = () => {
   return (
-    <div className="sm:mt-24 mt-16">
-      <section className="flex flex-col sm:flex-row sm:px-24 h-full px-6">
+    <div className="sm:mt-24 mt-16 bg-ec-blue">
+      <section className="flex flex-col sm:flex-row sm:px-24 h-full px-6 sm:my-0 my-6">
         <div className="flex flex-col sm:w-3/4 gap-4 justify-end sm:pb-24">
-          <span className="font-semibold text-4xl sm:text-8xl text-left text-ec-blue">
+          <span className="font-semibold text-4xl sm:text-8xl text-left text-white">
             Estrategia Consultores
           </span>
-          <span className="font-normal text-base sm:text-base sm:w-3/4 text-left">
+          <span className="font-normal text-base sm:text-base sm:w-3/4 text-left text-white">
             Centrada en investigación de mercados y opinión pública, planeación
             estratégica, comunicación integral e innovación para el
             emprendimiento
@@ -21,22 +23,23 @@ const Home = () => {
           />
           <button
             onClick={() => console.log("Consulta del Dia")}
-            className="hover:drop-shadow-[0_4px_80px_#5F6F94] flex w-80 justify-center items-center gap-4 font-sans tracking-wide sm:absolute bottom-0 bg-ec-blue z-20  px-8 py-4 rounded-md right-0"
+            type="button"
+            className="hover:drop-shadow-[0_4px_80px_#5F6F94] flex w-80 justify-center items-center gap-4 font-sans tracking-wide sm:absolute bottom-0 bg-white z-20  px-8 py-4 rounded-md right-0"
           >
             <img
               className="w-6 h-6 sm:h-auto sm:w-auto"
-              src="./assets/icons/question-answer.svg"
+              src="./assets/icons/question-answer-v2.svg"
               alt="icon button"
             />
-            <span className="text-white text-xs sm:text-sm font-sans font-bold uppercase">
+            <span className="text-ec-blue text-xs sm:text-sm font-sans font-bold uppercase">
               Responde la consulta del día
             </span>
           </button>
         </div>
       </section>
-      <section className="flex flex-col px-6 sm:px-24 py-6 h-full gap-6">
+      <section className="flex flex-col px-6 sm:px-24 py-8 h-full gap-6 bg-white sm:mt-16">
         <div className="flex">
-          <span className="uppercase font-poppins font-medium text-ec-blue">
+          <span className="uppercase font-poppins text-base font-medium text-ec-blue">
             Quiénes Somos
           </span>
         </div>
@@ -46,10 +49,10 @@ const Home = () => {
             style={{ backgroundImage: "url(./assets/img/img1.jpg)" }}
           ></div>
           <div className="flex flex-col sm:w-3/4 text-left justify-center gap-4">
-            <span className="font-poppins font-medium text-2x1">
+            <span className="font-poppins font-medium text-2x1 text-black">
               Empresa de Consultoría
             </span>
-            <span className="font-sans font-normal text-base">
+            <span className="font-sans font-normal text-base text-gray-700">
               ESTRATEGIA es una empresa de consultoría centrada en la generación
               de información y análisis de mercados, opinión pública, procesos
               de innovación y comunicación integral, para organizaciones
@@ -65,22 +68,25 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section className="flex flex-col px-6 sm:px-24 py-6 h-full gap-6">
+      <section className="flex flex-col pt-6 pb-20 sm:px-24 py-6 h-full gap-6">
         <div className="flex">
-          <span className="uppercase font-poppins font-medium text-ec-blue">
+          <span className="uppercase font-poppins font-medium text-base text-white">
             Nuestros Servcios
           </span>
         </div>
         <div className="flex flex-col sm:flex-row gap-4 sm:gap-16">
           <div className="flex flex-col sm:w-1/4 text-left justify-center gap-4">
-            <span className="font-poppins font-medium text-2x1">Ofrecemos</span>
-            <span className="font-sans font-normal text-base">
+            <span className="font-poppins font-medium text-2x1  text-white">
+              Ofrecemos
+            </span>
+            <span className="font-sans font-normal text-base  text-white">
               Servicios sobre investigación de mercados, análisis, diseño,
               planes y asesorías.
             </span>
             <button
               onClick={() => console.log("Servcios...")}
-              className="flex w-32 px-4 py-2 border border-ec-blue text-sm text-ec-blue font-sans font-semibold rounded-md justify-center mt:4 sm:mt-11 hover:text-white hover:bg-ec-blue"
+              type="button"
+              className="flex w-32 px-6 py-3 border bg-white border-ec-blue text-sm text-ec-blue font-sans font-semibold rounded-md justify-center mt:4 sm:mt-11 hover:text-black hover:bg-gray-300"
             >
               Saber Más
             </button>
@@ -89,6 +95,14 @@ const Home = () => {
             className="h-64 w-auto sm:h-96 rounded-sm sm:rounded-3xl bg-cover sm:w-3/4"
             style={{ backgroundImage: "url(./assets/img/img2.jpg)" }}
           ></div>
+        </div>
+      </section>
+      <section className="w-full flex flex-col justify-center items-center px-6 sm:px-24 py-8 bg-white">
+        <span className="w-full text-left uppercase font-poppins font-medium text-base text-ec-blue">
+          Estrategia en los medios
+        </span>
+        <div className="max-w-screen-xl flex flex-col sm:flex-row h-full gap-6 sm:mt-4 overflow-x-auto">
+          <Card />
         </div>
       </section>
     </div>
