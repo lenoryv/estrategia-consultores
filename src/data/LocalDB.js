6ba05e12b8bc24adbb026a9649f8890b43318c15
lenoryv <luisortiz141999@gmail.com>
2022-09-24 13:59:41 -0500

    Add Local Database

---
+++ src/data/LocalDB.js
@@ -0,0 +1,46 @@
+const Videos = [
+  {
+    id: "0",
+    url: "https://www.youtube.com/watch?v=HbpjNXmyUmg",
+    title: "Resultados del Estudio sobre el primer año de gestión del gobierno",
+    description:
+      "Bienvenidos a nuestra comunidad de YouTube. Somos Radio Pichincha, el otro relato.",
+  },
+  {
+    id: "1",
+    url: "https://www.youtube.com/watch?v=HbpjNXmyUmg",
+    title: "Resultados del Estudio sobre el primer año de gestión del gobierno",
+    description:
+      "Bienvenidos a nuestra comunidad de YouTube. Somos Radio Pichincha, el otro relato.",
+  },
+  {
+    id: "2",
+    url: "https://www.youtube.com/watch?v=HbpjNXmyUmg",
+    title: "Resultados del Estudio sobre el primer año de gestión del gobierno",
+    description:
+      "Bienvenidos a nuestra comunidad de YouTube. Somos Radio Pichincha, el otro relato.",
+  },
+  {
+    id: "3",
+    url: "https://www.youtube.com/watch?v=HbpjNXmyUmg",
+    title: "Resultados del Estudio sobre el primer año de gestión del gobierno",
+    description:
+      "Bienvenidos a nuestra comunidad de YouTube. Somos Radio Pichincha, el otro relato.",
+  },
+  {
+    id: "4",
+    url: "https://www.youtube.com/watch?v=HbpjNXmyUmg",
+    title: "Resultados del Estudio sobre el primer año de gestión del gobierno",
+    description:
+      "Bienvenidos a nuestra comunidad de YouTube. Somos Radio Pichincha, el otro relato.",
+  },
+  {
+    id: "5",
+    url: "https://www.youtube.com/watch?v=HbpjNXmyUmg",
+    title: "Resultados del Estudio sobre el primer año de gestión del gobierno",
+    description:
+      "Bienvenidos a nuestra comunidad de YouTube. Somos Radio Pichincha, el otro relato.",
+  },
+];
+
+export default Videos;
