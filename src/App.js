0d74452e94312aed8faa04131694e60a6a0bf6ed
lenoryv <luisortiz141999@gmail.com>
2022-09-24 21:01:18 -0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import './App.css';
 
 import { Footer } from './components/Footer';
 import { Navbar } from './components/Navbar';
+
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 //Pages
 import Home from './pages/Home';
+import Team from './pages/Team';
 
 function App() {
   return (
     <div className="App">
       <section className="App-content">
         <Navbar/>
-        <Home/>
-        <Footer/>
+        <BrowserRouter>
+          <Routes>
+            <Route index pathname="/" element={<Home/>}/>
+            <Route
+              path="/equipo"
+              element={<Team/>}
+            />
+          </Routes>
+        </BrowserRouter>
+        <Footer />
       </section>
     </div>
   );
