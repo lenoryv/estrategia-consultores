b4e6cc9a072ef5de7a75a1d9e3b55bd44e7c87aa
lenoryv <luisortiz141999@gmail.com>
2022-09-24 21:00:48 -0500

    Add new data

--- src/data/LocalDB.js
+++ src/data/LocalDB.js
@@ -1,4 +1,5 @@
-const Videos = [
+
+export const Videos = [
   {
     id: "0",
     url: "https://www.youtube.com/watch?v=HbpjNXmyUmg",
@@ -43,4 +44,60 @@ const Videos = [
   },
 ];
 
-export default Videos;
+export const Directivos = [
+  {
+    name: "Carlos Granda Tandazo",
+    position: "Director General",
+    description:
+      "Doctor (Ph.D.) en Comunicación Contemporánea por la Universidad Santiago de Compostela. Master en" +
+      "Marketing Estratégico por EOI Escuela de Negocios de Madrid. Diplomado en Entrepreneurship por el Babson College de EE.UU." +
+      " Especialista en Investigación de mercados y opinión pública.\n\nTrabajó como coordinador de proyectos en CEDATOS- Quito, a " +
+      "cargo de investigaciones  para cuentas como: Ecuador Boutling Company (Cola Cola), Bell South, Ecuasanitas, Dun & Bradstreet, " +
+      "etc.  Ex Director del departamento de Marketing de COOP MEGO en Loja.\n\nFundador y  director por ocho años del Departamento de " +
+      "Marketing y Publicidad Institucional de la UTPL. Docente-investigador de la misma Universidad por 23 años. Director de Educación" +
+      " Continua de la UTPL de 2007 a 2014 y de 2016 a 2018. Actualmente es coordinador de Grupo de Investigación en Innovación y " +
+      "Emprendimiento de UTPL. \nPresidente de la fundación CISOL, para educación a niños de sectores vulnerables.",
+  },
+  {
+    name: "Carla Granda Tandazo",
+    position: "Director General",
+    description:
+      "Master en Ciencia e Ingeniería de Alimentos por la Universidad Politécnica de Valencia." +
+      "Ingeniera en Industrias Alimenticias por la UTPL. Master en Diseño y Gestión de Proyectos" +
+      " por la UTPL. Ex Jefe de Innovación de productos de Discovery Trading Co. Guayaquil. " +
+      "Ex docente de la Universidad Católica de Guayaquil. Ex Jefe de Transformación productiva " +
+      "y Valor agregado en el Área de Producción del Gobierno Provincial de Loja. Representante de GLED ",
+  },
+];
+export const Asociados = [
+  {
+    name: "Aldo Salinas A.",
+    description:
+      "Ph. D. en Economía del Emprendimiento por la Universidad de Padua- Italia. " +
+      "Master en Economía por la Universidad de La Plata, Argentina. Docente de Pre y Posgrado de la UESS-Ecuador. ",
+  },
+  {
+    name: "Darío Vélez Jara  ",
+    description:
+      "Master en Economía por la Universidad de La Plata, Argentina. Economista de la UTPL." +
+      "Director Nacional de Estadística del INEC.",
+  },
+  {
+    name: "Juan P. Chauvin ",
+    description:
+      "PhD en Políticas Públicas y Master en Administración Pública y Desarrollo Internacional" +
+      " en la Universidad de Harvard, Master en Políticas Públicas en FLACSO-Ecuador, y títulos " +
+      "de B.A. en Sociología y Economía en la Universidad San Francisco de Quito",
+  },
+  {
+    name: "Armando Cabrera",
+    description:
+      "Master en Ciencias y Tecnología de la Información por la Universidad Politécnica de Madrid. " +
+      " Ingeniero en Sistemas Informáticos por la UTPL. Director de la Escuela de Ingeniería en informática de la UTPL.",
+  },
+  {
+    name: "Vismar G. Flores",
+    description:
+      "Ph.D. (c) Economía, mención Administración de la Universidad de Cuyo – Argentina.  Master en Administración de empresas por la UTEG – Ecuador ",
+  },
+];
