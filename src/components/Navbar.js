9bce345c72ac988ee802a706834702c54352c2a4
lenoryv <luisortiz141999@gmail.com>
2022-09-24 21:04:27 -0500

    Refactor navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,21 @@
+import React, { useEffect} from "react";
+
 function Navbar() {
+  const pathname = window.location.pathname;
+  let newStr = pathname.substring(1);
+
+  if (pathname==="/") newStr="home";
+  console.log(newStr);
+
+
+  useEffect(() => {
+    let menu = document.getElementsByClassName("block");
+    for (let i = 0; i < menu.length; i++) {
+      menu[i].style.color = "";
+    }
+    document.getElementById(newStr).style.color = "#001D6E";
+  }, [pathname, newStr]);
+
   return (
     <nav className="bg-white px-6 sm:px-16 py-2.5 dark:bg-gray-900 fixed w-full z-30 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
@@ -40,8 +57,9 @@ function Navbar() {
           <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-400 font-poppins">
             <li>
               <a
+                id="home"
                 href="/"
-                className="block py-2 pr-4 pl-3 text-white bg-blue-900 rounded md:bg-transparent md:text-blue-900 md:p-0 dark:text-white"
+                className="block py-2 pr-4 pl-3 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-900 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-400 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-400"
                 aria-current="page"
               >
                 Quiénes Somos
@@ -49,7 +67,8 @@ function Navbar() {
             </li>
             <li>
               <a
-                href="/"
+                id="equipo"
+                href="/equipo"
                 className="block py-2 pr-4 pl-3 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-900 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-400 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-400"
               >
                 Equipo
@@ -57,6 +76,7 @@ function Navbar() {
             </li>
             <li>
               <a
+                id="Services"
                 href="/"
                 className="block py-2 pr-4 pl-3 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-900 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-400 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-400"
               >
@@ -65,6 +85,7 @@ function Navbar() {
             </li>
             <li>
               <a
+                id="Customers"
                 href="/"
                 className="block py-2 pr-4 pl-3 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-900 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-400 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-400"
               >
@@ -73,6 +94,7 @@ function Navbar() {
             </li>
             <li>
               <a
+                id="Question"
                 href="/"
                 className="block py-2 pr-4 pl-3 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-900 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-400 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-400"
               >
@@ -85,4 +107,4 @@ function Navbar() {
     </nav>
   );
 }
- export { Navbar };
+export { Navbar };
