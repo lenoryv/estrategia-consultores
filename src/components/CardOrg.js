05cb98327d87535996abddfb284aa473a938a3cd
lenoryv <luisortiz141999@gmail.com>
2022-09-25 21:12:40 -0500

    refactor: add alt img

--- src/components/CardOrg.js
+++ src/components/CardOrg.js
@@ -7,7 +7,7 @@ function CardOrg({orgs}) {
           className="flex h-auto w-full sm:w-auto sm:max-w-md sm:max-h-40 p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
           <div className="flex items-center min-w-max h-full mr-4">
-            <img src={org.urlImage} className="w-24 h-24" />
+            <img src={org.urlImage} className="w-24 h-24" alt="Logo"/>
           </div>
           <div className="flex flex-col">
             <h5 className="flex mb-2 text-left text-2xl font-semibold font-poppins tracking-tight text-gray-900 dark:text-white">
