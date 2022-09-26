3e4a41195d77846b6b853621c7badaea9669dcd1
lenoryv <luisortiz141999@gmail.com>
2022-09-25 21:09:17 -0500

    add: database local

--- src/data/LocalDB.js
+++ src/data/LocalDB.js
@@ -87,3 +87,158 @@ export const Asociados = [
       "Ph.D. (c) Economía, mención Administración de la Universidad de Cuyo – Argentina.  Master en Administración de empresas por la UTEG – Ecuador ",
   },
 ];
+
+export const Organizaciones = [
+  {
+    name: "Universidad Técnica Particular de Loja (UTPL)",
+    urlImage:
+      "http://smartland.utpl.edu.ec/loja2030/sites/default/files/2019-02/logo_temp.png",
+    countries: "Loja - Ecuador",
+    years: "2014 – 2015 – 2016",
+  },
+  {
+    name: "Diario la Hora",
+    urlImage:
+      "https://img.genial.ly/60484908cbcc3a3386f6612a/627d5c4a-6965-40e1-9390-4305224ce1c4.png",
+    countries: "Loja, Machala, Ambato",
+    years: "2000- 2002- 2005 – 2009 – 2012- 2017",
+  },
+  {
+    name: "Delta Publicidad",
+    urlImage: "http://www.deltapublicidad.cl/logodelta.jpg",
+    countries: "Quito",
+    years: "2005- 2008 -2010 -2012- 2014",
+  },
+  {
+    name: "American Hospital",
+    urlImage: "https://sdaho.org/wp-content/uploads/2017/07/AHA_1.png",
+    countries: "Cuenca",
+    years: "2011",
+  },
+  {
+    name: "FAPECAFES",
+    urlImage:
+      "https://scontent.fuio5-1.fna.fbcdn.net/v/t39.30808-6/231238508_110748587922766_2645707627544111303_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=A0lCKEGu3ecAX9nZJ-z&_nc_oc=AQn6tb80Q8aJ2JjnTIfpGOwbh7owogIjzwkdsKxRuNkMNZtems7OiYk03daU8aCp6Wo&tn=NJoPJd9sev2iUMy6&_nc_ht=scontent.fuio5-1.fna&oh=00_AT_hLRYHphY-_PXF0K9DOWRE14rPW977k-6PjynkNFU9Xw&oe=63353F14",
+    countries: "Loja, Zamora",
+    years: "2008",
+  },
+  {
+    name: "CEDATOS",
+    urlImage:
+      "https://pbs.twimg.com/profile_images/881322749343526912/q_cgqBG4_400x400.jpg",
+    countries: "Loja, Zamora",
+    years: "2008",
+  },
+  {
+    name: "CACPE",
+    urlImage:
+      "https://cacpeloja.fin.ec/wp-content/uploads/2018/07/cacpe-loja-logo-sticky-2x.png",
+    countries: "Loja",
+    years: "2005 - 2010",
+  },
+  {
+    name: "DEPROSUR E. P.",
+    urlImage:
+      "https://icones.pro/wp-content/uploads/2021/04/icone-cercle-gris.png",
+    countries: "",
+    years: "2010",
+  },
+  {
+    name: "Banco de Loja",
+    urlImage:
+      "https://lagardservice.com.ec/wp-content/uploads/2015/02/LOGO-MAYOR-BANCO-DE-LOJA.png",
+    countries: "Loja",
+    years: "2009 - 2012",
+  },
+  {
+    name: "GEMESEG Seguridad",
+    urlImage:
+      "https://gemeseg.com/wp-content/uploads/2019/12/grupoempresarial2.png",
+    countries: "",
+    years: "2012 - 2022",
+  },
+  {
+    name: "Banco del Estado Regional Sur",
+    urlImage:
+      "https://icones.pro/wp-content/uploads/2021/04/icone-cercle-gris.png",
+    countries: "",
+    years: "2012 – 2015 - 2016",
+  },
+  {
+    name: "Clínica San Agustín",
+    urlImage:
+      "https://scontent.fuio5-1.fna.fbcdn.net/v/t1.6435-9/124636037_1653959318098191_2267847704066027921_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Jx57ij3dBpQAX_WZyJL&_nc_ht=scontent.fuio5-1.fna&oh=00_AT9hbUS3f67m18skZerla8zkgR27OzrpmDKxwzaIHkFZvA&oe=635622DF",
+    countries: "Loja",
+    years: "2015- 2019",
+  },
+  {
+    name: "Consultor Apoyo",
+    urlImage:
+      "https://scontent.fuio5-1.fna.fbcdn.net/v/t39.30808-6/305930746_446313747526795_2973143434750599683_n.png?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=2ImCgx6jOzwAX_behv6&_nc_ht=scontent.fuio5-1.fna&oh=00_AT-F7SSVgBc4sxnvl0JDJVdE8Z0LaSOnW6fxW0JPjm8S3Q&oe=63364B3A",
+    countries: "Quito",
+    years: "2014",
+  },
+  {
+    name: "Cooperativa de Transportes Loja",
+    urlImage:
+      "https://cooperativaloja.com.ec/wp-content/uploads/2020/02/Recurso-2.png",
+    countries: "Loja",
+    years: "2015 - 2019- 2021",
+  },
+  {
+    name: "Instituto Tecnológico Daniel Álvarez B.",
+    urlImage:
+      "https://scontent.fuio5-1.fna.fbcdn.net/v/t1.6435-9/54428310_1191577861003929_5068427946084007936_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=174925&_nc_ohc=tS7dRwWfbggAX8BxMQV&_nc_ht=scontent.fuio5-1.fna&oh=00_AT-t2UJAU2euGq1cWp3YCWpg766cV67DBqVRjohz2HnnOw&oe=635518B2",
+    countries: "Loja",
+    years: "2016",
+  },
+  {
+    name: "Corporación de Ferias de Loja",
+    urlImage:
+      "https://scontent.fuio5-1.fna.fbcdn.net/v/t39.30808-6/305327166_451307103703493_5552315801629658338_n.png?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=LeT-urCzfwUAX9T-O39&_nc_ht=scontent.fuio5-1.fna&oh=00_AT_r3zchE0k2_GkHdqFQqQco69Ted-MXvH2Kd1gfaJNgWA&oe=633599B3",
+    countries: "Loja",
+    years: "2017",
+  },
+  {
+    name: "Grupo Funerario Jaramillo",
+    urlImage:
+      "https://www.funerariajaramillo.com.ec/wp-content/uploads/2019/06/logo-funeraria2.png",
+    countries: "Loja",
+    years: "2018 - 2020",
+  },
+  {
+    name: "Ecotel TV ",
+    urlImage:
+      "https://directostv.teleame.com/wp-content/uploads/2017/10/Ecotel-TV-en-vivo-Online.png",
+    countries: "Loja",
+    years: "2018",
+  },
+  {
+    name: "Lev – Leb Publicitaria ",
+    urlImage:
+      "https://icones.pro/wp-content/uploads/2021/04/icone-cercle-gris.png",
+    countries: "Santiago de Chile",
+    years: "2018",
+  },
+  {
+    name: "Colegio Eugenio Espejo",
+    urlImage:
+      "https://www.uenikolastesla.com/wp-content/uploads/2019/06/cropped-LOGO-Eugenio-Espejo-2018-original-JPEG.jpg",
+    countries: "Loja",
+    years: "2021",
+  },
+  {
+    name: "GO Motors",
+    urlImage:
+      "https://scontent.fuio5-1.fna.fbcdn.net/v/t1.18169-9/29425680_339310829922715_1054395758637658800_n.png?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=v4VbK5l5H34AX9v2Qfl&tn=NJoPJd9sev2iUMy6&_nc_ht=scontent.fuio5-1.fna&oh=00_AT_r6lfThMsvaVDj8dwuvb-2MzH9GK8ourHHW-PBA4DWLg&oe=63557E6E",
+    countries: "",
+    years: "2020",
+  },
+  {
+    name: "MEDI HOSPITAL",
+    urlImage:
+      "https://medihospital.com.ec/wp-content/uploads/2020/05/mdh-logo-rectangular.svg",
+    countries: "Loja",
+    years: "2021",
+  },
+];
