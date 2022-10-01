18bb446098aeb37d432d6de97cc4e19f37a9c220
lenoryv <luisortiz141999@gmail.com>
2022-10-01 12:39:23 -0500

    refactor, style: footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,10 @@
 function Footer () {
     return (
-      <footer className="p-4 bg-white sm:px-16 sm:py-8 dark:bg-gray-900 w-full pt-6 sm:pt-16">
-        <div className="md:flex md:justify-between">
+      <footer className="p-4 bg-white sm:px-8 sm:py-8 dark:bg-gray-900 w-full pt-6 sm:pt-16">
+        <div className="md:flex md:justify-between sm:gap-8">
           <div className="mb-4 md:mb-0">
             <a href="/" className="flex items-center">
-              <img
-                src="./assets/logos/isotipo.svg"
-                className="mr-3 h-6 sm:h-9"
-                alt="EC Isotipo"
-              />
+              <img src="./assets/logos/isotipo.svg" alt="EC Isotipo" />
             </a>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
@@ -22,7 +18,8 @@ function Footer () {
                     href="https://www.google.com/maps/dir/?api=1&destination=-3.9968579812559%2C-79.198765313507&fbclid=IwAR3RinYqauTfjcjOlPUMrh4QwGjtPq4FMOACMPhKziW5rdVXLS96i0dhNTs"
                     className="hover:underline"
                   >
-                    10 de Agosto y Juan Jose Peña Loja, Ecuador
+                    Oficinas: Loja, calle Juan José Peña entre 10 de Agosto y
+                    Rocafuerte, Piso 2.
                   </a>
                 </li>
               </ul>
@@ -48,17 +45,26 @@ function Footer () {
               </h2>
               <ul className="text-gray-600 dark:text-gray-400 text-base text-left">
                 <li className="mb-4">
-                  <a href="/" className="hover:underline">
+                  <a
+                    href="https://api.whatsapp.com/send/?phone=593998200312"
+                    className="hover:underline"
+                  >
                     Telf: (+593) 99 820 0312
                   </a>
                 </li>
                 <li className="mb-4">
-                  <a href="/" className="hover:underline">
-                    Telf: (+593) 99 820 0312
+                  <a
+                    href="https://api.whatsapp.com/send/?phone=593997909371"
+                    className="hover:underline"
+                  >
+                    Telf: (+593) 99 790 9371
                   </a>
                 </li>
                 <li className="mb-4">
-                  <a href="/" className="hover:underline">
+                  <a
+                    href="mailto:estrategialoja@gmail.com"
+                    className="hover:underline"
+                  >
                     Correo: estrategialoja@gmail.com
                   </a>
                 </li>
