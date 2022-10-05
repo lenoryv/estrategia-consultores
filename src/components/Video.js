d009a473c429fd9a51382381f1269e40fcbbf40e
lenoryv <luisortiz141999@gmail.com>
2022-10-05 13:58:18 -0500

    style: card video

--- src/components/Video.js
+++ src/components/Video.js
@@ -3,7 +3,7 @@ function Video() {
     <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
       <a href="/">
         <iframe
-          className="w-full sm:w-full h-64 rounded-lg"
+          className="w-full sm:w-full h-[354px] rounded-lg"
           src="https://www.youtube.com/embed/SN3t8qiTa2Q"
           title="Cómo incorporar videos y listas de reproducción"
           frameborder="0"
