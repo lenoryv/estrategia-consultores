9d4935d78100b53a33b3651d92b713fe626d070d
lenoryv <luisortiz141999@gmail.com>
2022-10-06 12:58:26 -0500

    style: width

--- src/pages/Survey.js
+++ src/pages/Survey.js
@@ -5,11 +5,8 @@ const Survey = () => {
         <iframe
           title="Consulta del Dia"
           src="https://docs.google.com/forms/d/e/1FAIpQLSeUXjWWTloAm9phaqR4CNJCLnqonB9bv_D3wIHlH1xCt6fZZA/viewform?embedded=true"
-          width="100%"
-          height="100%"
-          frameborder="0"
-          marginheight="0"
-          marginwidth="0"
+          width="90%"
+          height="85%"
         >
           Cargando…
         </iframe>
