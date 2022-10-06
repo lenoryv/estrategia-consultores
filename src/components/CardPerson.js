7c4a704af9780e90c8b362ac7707f1de4a0698ee
lenoryv <luisortiz141999@gmail.com>
2022-10-06 17:27:32 -0500

    style: card person

--- src/components/CardPerson.js
+++ src/components/CardPerson.js
@@ -3,20 +3,20 @@ function CardPerson({ people }) {
     return (
       <div
         key={person.name}
-        className="flex flex-col sm:flex-row items-start  gap-6 sm:w-auto sm:max-h-min p-6 sm:p-8 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        className="flex flex-col md:flex-row items-start  gap-6 md:w-auto md:max-h-min p-6 md:p-8 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
       >
-        <div className="flex flex-col sm:w-3/4 sm:max-h-max">
-          <h5 className="mb-2 text-left text-2xl font-bold font-poppins tracking-tight text-gray-900 dark:text-white">
+        <div className="flex flex-col md:w-3/4 md:max-h-max">
+          <h5 className="mb-2 text-left text-xl font-bold font-poppins tracking-tight text-gray-900 dark:text-white">
             {person.name}
           </h5>
-          <h6 className="mb-2 text-xl font-semibold font-sans text-left tracking-tight text-gray-900 dark:text-white">
+          <h6 className="mb-2 text-lg font-semibold font-sans text-left tracking-tight text-gray-900 dark:text-white">
             {person?.position}
           </h6>
-          <p className="leading-relaxed whitespace-pre-line font-normal font-sans text-base text-left text-gray-700 dark:text-gray-400">
+          <p className="leading-relaxed whitespace-pre-line font-normal font-sans text-sm text-left text-gray-700 dark:text-gray-400">
             {person.description}
           </p>
         </div>
-        <div className="flex flex-col sm:w-[400px] sm:h-[400px] rounded-lg overflow-hidden">
+        <div className="flex flex-col md:w-[240px] md:h-[320px] rounded-lg overflow-hidden">
           <img src={person?.img} alt="Person" />
         </div>
       </div>
