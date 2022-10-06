33ebdb11dd5fb69cf99dacf5c286f8e7af85b029
lenoryv <luisortiz141999@gmail.com>
2022-10-06 17:42:04 -0500

    style: width and height form

--- src/pages/Survey.js
+++ src/pages/Survey.js
@@ -3,10 +3,9 @@ const Survey = () => {
     <div className="w-full h-screen dark:bg-gray-900">
       <section className="mt-20 sm:mt-24 h-full">
         <iframe
+          className="md:w-11/12 w-full h-5/6"
           title="Consulta del Dia"
           src="https://docs.google.com/forms/d/e/1FAIpQLSeUXjWWTloAm9phaqR4CNJCLnqonB9bv_D3wIHlH1xCt6fZZA/viewform?embedded=true"
-          width="90%"
-          height="85%"
         >
           Cargando…
         </iframe>
