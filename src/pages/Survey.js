7ba3f1f8ba80dc2aff06bc695914786d37ab1e9f
lenoryv <luisortiz141999@gmail.com>
2022-10-08 10:37:48 -0500

    style: height survey

--- src/pages/Survey.js
+++ src/pages/Survey.js
@@ -3,7 +3,7 @@ const Survey = () => {
     <div className="w-full h-screen dark:bg-gray-900">
       <section className="mt-20 sm:mt-24 h-full">
         <iframe
-          className="md:w-11/12 w-full h-5/6"
+          className="md:w-11/12 w-full h-full"
           title="Consulta del Dia"
           src="https://docs.google.com/forms/d/e/1FAIpQLSeUXjWWTloAm9phaqR4CNJCLnqonB9bv_D3wIHlH1xCt6fZZA/viewform?embedded=true"
         >
