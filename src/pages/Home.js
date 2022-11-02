ffa4d71e6554853d5b7677eea398426f13bb8bad
lenoryv <luisortiz141999@gmail.com>
2022-11-02 16:54:10 -0500

    style: home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,58 +6,58 @@ import { BtnService} from '../components/BtnService';
 
 const Home = () => {
   return (
-    <div className="sm:mt-18 mt-14 bg-ec-blue dark:bg-gray-900">
+    <div className="sm:mt-18 mt-14 pt-6 bg-ec-blue dark:bg-gray-900">
       <section className="flex flex-col sm:flex-row sm:px-24 h-full px-6 sm:my-0 my-6">
-        <div className="flex flex-col sm:w-3/4 gap-4 justify-end sm:pb-0">
-          <span className="font-extrabold font-montserrat text-4xl sm:text-6xl text-left text-white uppercase">
+        <div className="flex flex-col sm:w-3/4 gap-4 justify-end sm:pb-6">
+          <span className="font-extrabold font-montserrat text-4xl sm:text-5xl text-left text-white uppercase">
             Estrategia Consultores
           </span>
           <span className="font-light text-base sm:text-base sm:w-3/4 text-left text-white">
             Es una empresa dedicada al análisis de mercados y estudios de
             opinión pública.
             <br />
-            Realizamos además asesoría empresarial en innovación, planeación,
+            Realizamos además asesoría empresarial en innovación, emprendimiento, planeación,
             marketing y comunicación integral.
           </span>
         </div>
         <div className="flex flex-col items-center gap-10 pt-8 sm:mt-0">
           <img
-            className="z-10 hidden sm:flex w-64 h-64"
+            className="z-10 hidden sm:flex w-72 h-72"
             src="./assets/logos/isotipo-metallic.svg"
             alt="Isotipo"
           />
           <a
             href="/encuesta"
             type="button"
-            className="hover:drop-shadow-[0_4px_80px_#5F6F94] flex w-80 items-center font-sans tracking-wide bg-gray-100 z-20 px-8 py-4 rounded-md sm:rounded-xl border border-gray-900"
+            className="hover:drop-shadow-[0_4px_80px_#5F6F94] flex w-full items-center font-sans tracking-wide bg-gray-100 z-20 px-4 py-2 rounded-md sm:rounded-lg border border-gray-900"
           >
             <img
-              className="w-6 h-6 sm:h-auto sm:w-10"
+              className="w-6 h-6 sm:h-auto sm:w-8"
               src="./assets/icons/question-answer-v2.svg"
               alt="icon button"
             />
-            <span className="text-center text-ec-blue text-xs sm:text-base font-montserrat font-bold">
+            <span className="w-80 text-center text-ec-blue text-xs sm:text-sm font-montserrat font-semibold">
               Responde nuestra encuesta de hoy
             </span>
           </a>
         </div>
       </section>
-      <section className="flex flex-col px-6 sm:px-24 py-8 h-full gap-6 bg-white sm:mt-16 dark:bg-gray-900">
+      <section className="flex flex-col px-6 sm:px-24 py-8 h-full gap-6 bg-white sm:mt-10 dark:bg-gray-900">
         <div className="flex sm:grid w-full flex-col sm:flex-row gap-4 sm:gap-8 grid-cols-3 items-center">
           <div className="flex flex-col w-full justify-start items-start gap-4">
             <span className="uppercase font-montserrat text-base font-medium text-ec-blue dark:text-white">
               Quiénes Somos
             </span>
-            <span className="flex font-sans font-normal text-left text-sm text-gray-700 dark:text-white sm:mr-6 leading-relaxed">
+            <p className="flex font-sans font-normal text-justify text-sm text-gray-700 dark:text-white sm:mr-6 leading-relaxed">
               Constituimos un grupo consultor multidisciplinario integrado por
               profesionales con más de 22 años de experiencia en las áreas de
-              Opinión Pública, Investigación de Mercados, Economía, Marketing,
+              Opinión Pública, Investigación de Mercados, Economía, Emprendimiento, Marketing,
               Comunicación, Proyectos y Estadística, trabajando para empresas
               nacionales e internacionales de reconocida trayectoria. Contamos
               con oficinas en Loja y Quito, además de representantes en
               Guayaquil y Cuenca, con lo cual se garantiza una capacidad
               nacional de operación.
-            </span>
+            </p>
           </div>
           <div className="w-full">
             <Video
@@ -71,7 +71,7 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section className="flex flex-col pt-6 pb-8 sm:pt-10 sm:px-24 px-6 py-6 h-full gap-6 sm:gap-0 bg-slate-100">
+      <section className="flex flex-col pt-6 pb-8 sm:pt-10 sm:px-24 px-6 py-6 h-full gap-6 sm:gap-0 bg-slate-50">
         <div className="flex flex-col sm:flex-row gap-4 sm:gap-16">
           <div className="flex flex-col sm:w-1/4 text-left justify-center gap-4 sm:gap-8 items-center sm:items-start">
             <span className="flex uppercase font-montserrat font-medium text-base text-ec-blue">
