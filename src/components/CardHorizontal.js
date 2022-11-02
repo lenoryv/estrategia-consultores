302cc879cd6af3315f834b4fdb8f5fd5d0c3cc29
lenoryv <luisortiz141999@gmail.com>
2022-11-02 16:53:15 -0500

    style: card horizontal

--- src/components/CardHorizontal.js
+++ src/components/CardHorizontal.js
@@ -3,7 +3,7 @@ function CardHorizontal({ Services }) {
     return (
       <div
         key={service.name}
-        className="flex flex-col text-left items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+        className="flex flex-col min-w-max text-left items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
         style={{ boxShadow: `0 10px 15px -3px ${service.color}` }}
       >
         <img
@@ -12,10 +12,10 @@ function CardHorizontal({ Services }) {
           alt="Service"
         />
         <div className="flex flex-col justify-between p-4 leading-normal">
-          <h5 className="mb-2 text-base font-bold font-montserrat tracking-tight text-gray-900 dark:text-white">
+          <h5 className="w-96 mb-2 text-base font-bold font-montserrat tracking-tight text-gray-900 dark:text-white">
             {service.name}
           </h5>
-          <p className="mb-3 font-normal text-sm font-sans text-gray-700 dark:text-gray-400">
+          <p className="w-96 mb-3 font-normal text-xs font-sans text-gray-700 dark:text-gray-400">
             {service.description}
           </p>
         </div>
