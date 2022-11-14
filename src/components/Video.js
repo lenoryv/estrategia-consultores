79e5f758209c72998dbb1f1a851c4bb891dcd07c
lenoryv <luisortiz141999@gmail.com>
2022-11-13 20:24:23 -0500

    refactor: display hidden video

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,6 +1,6 @@
 function Video({ video }) {
   return (
-    <div className="hidden sm:flex w-auto sm:h-72 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <div className="w-auto sm:h-72 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
       <a href="https://www.youtube.com/embed/P-ltn1kC9EY?controls=0&amp;start=435&end=612">
         <div className="flex px-4 pt-3 pb-1">
           <p className="flex text-left mb-2 font-sans font-semibold text-sm sm:text-base text-gray-700 dark:text-gray-400">
@@ -9,7 +9,7 @@ function Video({ video }) {
         </div>
       </a>
       <iframe
-        className="w-full h-3/4 rounded-b-lg"
+        className="hidden sm:flex w-full h-3/4 rounded-b-lg"
         sandbox="allow-scripts allow-same-origin allow-popups-to-escape-sandbox"
         loading="lazy"
         src={video}
